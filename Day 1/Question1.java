91ad9bb27c4315cea7d989a218ad8b63b77ad345
Aadarsh Bansal <ab6582@srmist.edu.in>
2025-05-19 15:00:00 +0530

    Aadarsh

---
+++ Day 1/Question1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Question1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World. \nHello, Java.");
+    }
+}
---
+++ Day 1/Question2.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
